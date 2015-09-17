1a1082e65a24ec4967dcf5ff7262de0c169c6d3c
479245760 <479245760@qq.com>
2015-09-17 18:15:46 +0800

    first commit and git init

---
+++ src/Mrlian/Smsservice/SmsserviceServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Mrlian\Smsservice;
+
+use Illuminate\Support\ServiceProvider;
+
+class SmsserviceServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('mrlian/smsservice');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
